3113f4a2a5f74b5e2e99b1581c088e5ccb37184c
Philip Betzler <pb082@hdm-stuttgart.de>
2021-08-04 19:10:41 +0200

    trying to get the exe working and fixing befor deploy script V3

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -20,20 +20,20 @@ main() {
     # TODO Update this to build the artifacts that matter to you
     cross build --target $TARGET --release
 
-	case $TRAVIS_OS_NAME in
-        linux)
+	case $TARGET in
+        x86_64-unknown-linux-gnu)
 			cp target/$TARGET/release/lisp_interpreter $stage/
 
 			cd $stage
 			tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
             ;;
-		osx)
+		x86_64-apple-darwin)
             cp target/$TARGET/release/lisp_interpreter $stage/
 
 			cd $stage
 			tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
             ;;
-		windows)
+		x86_64-pc-windows-gnu)
 			cp target/$TARGET/release/lisp_interpreter.exe $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.exe
 			cd $stage
 			;;
